39ab81d68e1930f6277c4130eed2ae0927099cdc
fermin romero <ferminromero@hotmail.com>
2022-10-20 19:25:13 -0300

    React Primera Entrega

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import NavBar from "./components/navBar/NavBar"
+import '../src/style.css'
+import ItemListContainer from "./components/itemListContainer/itemListContainer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Nosotros from "./components/nosotros/nosotros";
+import Contactos from "./components/contactos/contactos";
+import ItemDetailContainer from "./components/itemDetailContainer/itemDetailContainer";
+import CartProvider from "./context/cartProvider";
+import Footer from "./components/footer/footer";
+
+function App() {
+
+  return (
+    <div>
+    
+      <BrowserRouter>
+        <NavBar></NavBar>
+        <Routes>
+          <Route path="/" element={<ItemListContainer />} />
+          <Route path="/FundApp-Caramaschi/" element={<ItemListContainer/>}/>
+          <Route path="/nosotros" element={<Nosotros />} />
+          <Route path="/contactos" element={<Contactos />} />
+          <Route path="/:categoria/:titulo" element={<ItemDetailContainer/>} />
+          <Route path= "/categoria/:category" element={<ItemListContainer/>}/>
+        </Routes>
+        <Footer></Footer>
+      </BrowserRouter>
+    </div>
+   
+
+  );
+
+}
+export default App;
---
+++ src/components/menuWidget/menuWidget.jsx
@@ -0,0 +1,40 @@
+import { useRef } from "react";
+import { Link } from "react-router-dom";
+import './menu-widget.css'
+
+const MenuWidget = () => {
+    const cerrarMenu = () => {
+        cerrarOpciones.current.classList.remove("desactivar-opciones")
+    }
+    const abrirMenu = () => {
+        cerrarOpciones.current.classList.add("desactivar-opciones")
+    }
+
+    const cerrarOpciones = useRef(null)
+
+    const abrirOpcion = useRef(null)
+
+
+
+    return (
+        <>
+            <i onClick={abrirMenu} className='bx bx-md bx-menu' ></i>
+            <div className='opciones' ref={cerrarOpciones}>
+                <i onClick={cerrarMenu} className='bx bx-x cerrar-opciones'></i>
+                <h3>FundApp</h3>
+                <ul>
+                    <Link to={"/"}><li>Inicio</li></Link>
+                    <Link to={"/nosotros"}><li>Nosotros</li></Link>
+                    <Link to={"/"}><li>Fundas</li></Link>
+                    <div ref={abrirOpcion}>
+                        <Link to={"/categoria/dibujadas"}><div className="categorias">Dibujadas</div></Link>
+                        <Link to={"/categoria/solidas"}><div className="categorias">Solidas</div></Link>
+                        <Link to={"/categoria/tematicas"}><div className="categorias">Tematicas</div></Link>
+                    </div>
+                    <Link to={"/contactos"}><li>Contactos</li></Link>
+                </ul>
+            </div>
+        </>
+    )
+}
+export default MenuWidget;
